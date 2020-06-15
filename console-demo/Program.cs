23121246dd462e4740eb419b58ad7ea2b4eedab4
Jay T <jaytula@gmail.com>
2020-06-15 14:16:10 -0700

    Intro to C Sharp: Work with strings

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -13,6 +13,14 @@ namespace console_demo
 
       Console.WriteLine("Hello " + aFriend);
       Console.WriteLine($"Hello {aFriend}");
+
+      string firstFriend = "Maria";
+      string secondFriend = "Sage";
+      Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+      Console.WriteLine($"The name {firstFriend} had {firstFriend.Length} letters.");
+      Console.WriteLine($"The name {secondFriend} had {secondFriend.Length} letters.");
+
     }
   }
 }
