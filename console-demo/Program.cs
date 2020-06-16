e5c1497eac13dadf9e20b24bc700a00835a48e15
Jay T <jaytula@gmail.com>
2020-06-15 20:08:52 -0700

    Intro to C Sharp: Do more with strings

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -21,6 +21,19 @@ namespace console_demo
       Console.WriteLine($"The name {firstFriend} had {firstFriend.Length} letters.");
       Console.WriteLine($"The name {secondFriend} had {secondFriend.Length} letters.");
 
+      // Do more with strings
+      string greeting = "     Hello World!      ";
+      Console.WriteLine($"[{greeting}]");
+      Console.WriteLine($"[{greeting.TrimStart()}]");
+      Console.WriteLine($"[{greeting.TrimEnd()}]");
+      Console.WriteLine($"[{greeting.Trim()}]");
+
+      string sayHello = "Hello World";
+      Console.WriteLine(sayHello);
+      Console.WriteLine(sayHello.Replace("Hello", "Greetings"));
+      Console.WriteLine(sayHello.ToUpper());
+      Console.WriteLine(sayHello.ToLower());
+
     }
   }
 }
