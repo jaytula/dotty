81946f8f2f83ea7cd4d699726a44bcf29b0a3c0e
Jay T <jaytula@gmail.com>
2020-06-15 21:59:07 -0700

    Intro to C Sharp: Search strings

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -34,6 +34,16 @@ namespace console_demo
       Console.WriteLine(sayHello.ToUpper());
       Console.WriteLine(sayHello.ToLower());
 
+      // Search strings
+      string songLyrics = "You say goodbye, and I say hello";
+      Console.WriteLine(songLyrics.Contains("goodbye"));
+      Console.WriteLine(songLyrics.Contains("greetings"));
+
+      // Search strings exercise
+      Console.WriteLine(songLyrics.StartsWith("You"));
+      Console.WriteLine(songLyrics.StartsWith("goodbye"));
+      Console.WriteLine(songLyrics.EndsWith("hello"));
+      Console.WriteLine(songLyrics.EndsWith("goodbye"));
     }
   }
 }
