d3a000c504952463938040e755f6aecd30bbadde
Jay T <jaytula@gmail.com>
2020-06-17 15:37:23 -0700

    Branches: Make descirions using the if statement

--- console-demo/BranchesAndLoops.cs
+++ console-demo/BranchesAndLoops.cs
@@ -1,11 +1,21 @@
 using System;
 
-namespace console_demo {
+namespace console_demo
+{
   class BranchesAndLoops
   {
     public static void BranchesAndLoopsMain()
     {
       Console.WriteLine("Branches and Loops");
+
+      int a = 5;
+      int b = 6;
+      if (a + b > 10)
+        Console.WriteLine("The answer is greater than 10");
+
+      b = 3;
+      if (a + b > 10)
+        Console.WriteLine("The answer is greater than 10");
     }
   }
 }
