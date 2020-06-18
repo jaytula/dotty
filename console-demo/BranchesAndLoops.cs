cbe744ca237cc9a38755cd36d7fbba61bd72e994
Jay T <jaytula@gmail.com>
2020-06-17 19:31:06 -0700

    Branches: Make if and else work together

--- console-demo/BranchesAndLoops.cs
+++ console-demo/BranchesAndLoops.cs
@@ -16,6 +16,31 @@ namespace console_demo
       b = 3;
       if (a + b > 10)
         Console.WriteLine("The answer is greater than 10");
+
+      // Make if and else work together
+      a = 5;
+      b = 3;
+
+      if (a + b > 10)
+      {
+        Console.WriteLine("The answer is greater than 10");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10");
+      }
+
+      int c = 4;
+      if ((a + b + c > 10) || (a == b))
+      {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("Or the first number is equal to the second");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("And the first number is not equal to the second");
+      }
     }
   }
 }
