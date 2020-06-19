4b45966f97504b94c80b1ffaeb532cd09b28c603
Jay T <jaytula@gmail.com>
2020-06-19 10:02:26 -0700

    List Collection: Create lists

--- console-demo/ListCollection.cs
+++ console-demo/ListCollection.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Collections.Generic;
 
-namespace console_demo {
-  class ListCollection {
+namespace console_demo
+{
+  class ListCollection
+  {
     public static void ListCollectionMain()
     {
-      Console.WriteLine("ListCollectionMain");
+      var names = new List<string> { "<name>", "Ana", "Felipe" };
+
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
     }
   }
 }
