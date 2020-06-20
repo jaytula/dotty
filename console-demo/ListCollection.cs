fbf3a28bcfb5304a1dfc5bfb8d7d626c816ed724
Jay T <jaytula@gmail.com>
2020-06-19 20:50:22 -0700

    List Collection: Search and sort lists

--- console-demo/ListCollection.cs
+++ console-demo/ListCollection.cs
@@ -29,6 +29,19 @@ namespace console_demo
       Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
 
       Console.WriteLine($"The list has {names.Count} people in it");
+
+      // Search and sort lists
+      var index = names.IndexOf("Felipe");
+      if(index != -1) 
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+
+      var notFound = names.IndexOf("Not Found");
+      Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+      names.Sort();
+      foreach(var name in names) {
+        Console.WriteLine($"Hello {name.ToUpper()}");
+      }
     }
   }
 }
