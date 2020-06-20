e9797733babc80df8851aec59d49ef1873d37ba5
Jay T <jaytula@gmail.com>
2020-06-20 12:31:33 -0700

    List Collection: List of other types

--- console-demo/ListCollection.cs
+++ console-demo/ListCollection.cs
@@ -32,16 +32,29 @@ namespace console_demo
 
       // Search and sort lists
       var index = names.IndexOf("Felipe");
-      if(index != -1) 
+      if (index != -1)
         Console.WriteLine($"The name {names[index]} is at index {index}");
 
       var notFound = names.IndexOf("Not Found");
       Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
 
       names.Sort();
-      foreach(var name in names) {
+      foreach (var name in names)
+      {
         Console.WriteLine($"Hello {name.ToUpper()}");
       }
+
+      // LIst of other types
+
+      var fibonacciNumbers = new List<int> { 1, 1 };
+
+      var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+      var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+      fibonacciNumbers.Add(previous + previous2);
+
+      foreach(var item in fibonacciNumbers)
+        Console.WriteLine(item);
     }
   }
 }
