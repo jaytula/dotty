1900a2720e638dad3e1754feb07644843c2f386c
Jay T <jaytula@gmail.com>
2020-06-20 20:07:09 -0700

    Intro to Classes: Open a new account

--- console-oop-demo/BankAccount.cs
+++ console-oop-demo/BankAccount.cs
@@ -4,9 +4,18 @@ namespace console_oop_demo
 {
   public class BankAccount
   {
+    private static int accountNumberSeed = 1234567890;
     public string Number { get; }
     public string Owner { get; set; }
     public decimal Balance { get; }
+
+
+    public BankAccount(string name, decimal initialBalance) {
+      this.Owner = name;
+      this.Balance = initialBalance;
+      this.Number = accountNumberSeed.ToString();
+      accountNumberSeed++;
+    }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
--- console-oop-demo/Program.cs
+++ console-oop-demo/Program.cs
@@ -2,11 +2,13 @@
 
 namespace console_oop_demo
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      var account = new BankAccount("<name>", 1000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
     }
+  }
 }
