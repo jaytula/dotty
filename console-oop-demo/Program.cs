fc0256ced55d608ef838ad66cf79507c5dc60aa2
Jay T <jaytula@gmail.com>
2020-06-22 17:45:41 -0700

    Intro to classes: Challenge - log all transactions

--- console-oop-demo/Program.cs
+++ console-oop-demo/Program.cs
@@ -14,25 +14,27 @@ namespace console_oop_demo
       account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
       Console.WriteLine(account.Balance);
 
-      // Test initializing with negative balance
-      try
-      {
-        var invalidAccount = new BankAccount("invalid", -55);
-      }
-      catch (ArgumentOutOfRangeException e)
-      {
-        Console.WriteLine("Exception caught creating account with negative balance");
-        Console.WriteLine(e.ToString());
-      }
-      try
-      {
-        account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-      }
-      catch (InvalidOperationException e)
-      {
-        Console.WriteLine("Exception caught trying to overdraw");
-        Console.WriteLine(e.ToString());
-      }
+      // // Test initializing with negative balance
+      // try
+      // {
+      //   var invalidAccount = new BankAccount("invalid", -55);
+      // }
+      // catch (ArgumentOutOfRangeException e)
+      // {
+      //   Console.WriteLine("Exception caught creating account with negative balance");
+      //   Console.WriteLine(e.ToString());
+      // }
+      // try
+      // {
+      //   account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+      // }
+      // catch (InvalidOperationException e)
+      // {
+      //   Console.WriteLine("Exception caught trying to overdraw");
+      //   Console.WriteLine(e.ToString());
+      // }
+
+      Console.WriteLine(account.GetAccountHistory());
     }
   }
 }
