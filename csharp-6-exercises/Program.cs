94c80a5d52c135cc6babed7b93b77ff58d5eaa1b
Jay T <jaytula@gmail.com>
2020-06-22 20:31:48 -0700

    CSharp 6: Read-only auto-properties enable read-only types

--- csharp-6-exercises/Program.cs
+++ csharp-6-exercises/Program.cs
@@ -2,11 +2,36 @@
 
 namespace csharp_6_exercises
 {
-    class Program
+  class Person
+  {
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public Person(string firstName, string lastName)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      FirstName = firstName;
+      LastName = lastName;
     }
+
+    public string AllCaps()
+    {
+      return ToString().ToUpper();
+    }
+
+    public override string ToString()
+    {
+      return $"{FirstName} {LastName}";
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var person = new Person("Max", "Schwartz");
+      Console.WriteLine($"The name, in all caps: " + person.AllCaps());
+      Console.WriteLine($"The name is {person.ToString()}");
+    }
+  }
 }
