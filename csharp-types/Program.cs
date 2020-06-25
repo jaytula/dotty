773b4352af0485a3a14667a089190e078b9fda1b
Jay T <jaytula@gmail.com>
2020-06-24 21:05:26 -0700

    Add sub-project csharp-types

---
+++ csharp-types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
