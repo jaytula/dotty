ebe7f40fac93061ee567f417d2c28d13d36827f0
Jay T <jaytula@gmail.com>
2020-06-30 22:11:40 -0700

    Razor Pages: Add a data model (rest...)
    - Add NuGet packages and EF tools
    - Add a database context class
    - Add a database connection string
    - Register the database context

--- razor-pages-tutorial/Models/Movie.cs
+++ razor-pages-tutorial/Models/Movie.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace RazorPagesMovie.Model
+namespace RazorPagesMovie.Models
 {
   public class Movie
   {
--- razor-pages-tutorial/Program.cs
+++ razor-pages-tutorial/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace razor_pages_tutorial
+namespace RazorPagesMovie
 {
     public class Program
     {
